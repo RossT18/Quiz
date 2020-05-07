22964a2f347f9c2af42baad95c3346ae2671a205
Ross Tinsley <rtinsley1199@gmail.com>
2020-05-07 02:11:08 +0100

    Add spinning wheel (no functionality yet)

--- js/script.js
+++ js/script.js
@@ -13,20 +13,82 @@ const questionLetters = [
     'D. '
 ];
 
+let categories = [];
+const validCategories = [
+    'General Knowledge',
+    'Geography',
+    'Science',
+    'Sports',
+    'Entertainment',
+    'History'
+];
+
+class Category {
+    constructor(name, ids) {
+        this._name = name;
+        this._ids = ids;
+    }
+
+    get name() {
+        return this._name;
+    }
+    get ids() {
+        return this._ids;
+    }
+    get firstId() {
+        if (this._ids.length > 0) {
+            return this._ids[0];
+        }
+        else {
+            return undefined;
+        }
+    }
+}
+
+
 $.ajaxSetup({ async: false }); // Probably should deal with async methods properly but this will do, for now.
+
+$.getJSON('https://opentdb.com/api_category.php', function(data) {
+    const categoriesRes = data.trivia_categories;
+    for (let i = 0; i < categoriesRes.length; i++) {
+        // For each category retrieved.
+
+        let thisCategory = new Category(categoriesRes[i].name, categoriesRes[i].id);
+        categories.push(thisCategory);
+
+        
+
+
+        /*
+
+        const catName = categoriesRes[i].name;
+        const catId = categoriesRes[i].id;
+        
+        validCategories.forEach(validCat => {
+            if (validCat === catName) {
+                // Name is exactly the same. Therefore only 1
+                let catObj = {name: catName, ids: [catId]}
+            }
+            else if (catName.startsWith(validCat)) {
+                // It's got sub-categories because it only starts with a valid category name
+                categories.forEach(cat => {
+                    if (cat.name === catName) {
+                        // Already added to categories. Just add 1.
+                    }
+                });
+            }
+        });*/
+    }
+});
+
+// {name, ids[]}
 
 //#region Loading Category Information
 /*
 let showingInfo = false;
 const categories = [];
 
-$.getJSON('https://opentdb.com/api_category.php', function(data) {
-    const categoriesRes = data.trivia_categories;
-    for (let i = 0; i < categoriesRes.length; i++) {
-        const category = categoriesRes[i];
-        categories[i] = category;
-    }
-});
+
 
 function makeTable(headers, id) {
     let table  = document.createElement('table');
@@ -282,4 +344,75 @@ $("#startBtn").click(function() {
     questions = loadQuestions(token, 10);
     // Show the question, 4 options as buttons
     showQuestion();
+});
+
+function unflap() {
+    $("#flapper").animate(
+        { deg: 0 },
+        {
+            duration: 40,
+            step: function(now) {
+                $(this).css({ transform: `rotate(${now}deg)`});
+            }
+        }
+    );
+}
+
+function flap() {
+    $("#flapper").animate(
+        { deg: -50 },
+        {
+            duration: 60,
+            complete: unflap,
+            step: function(now) {
+                $(this).css({ transform: `rotate(${now}deg)`});
+            }
+        }
+    );
+}
+
+function spinWheel() {
+    let fullSpins =  getRandomInt(2, 6);
+    let angles = [];
+    let currentAngle = 360;
+    validCategories.forEach(category => {
+        let angleObj = {name: category, angle: currentAngle};
+        angles.push(angleObj);
+        currentAngle -= 60;
+    });
+    
+    let catIndex = getRandomInt(0, angles.length);
+    let randCat = angles[catIndex];
+
+    let sectorRange = getRandomInt(0, 44) - 22; // Offset by 22 for ahead and after centre of sector.
+
+    let targetAngle = (fullSpins * 360) + randCat.angle + sectorRange;
+    const range = 3;
+    let hasFlapped = false;
+    $("#wheel").animate(
+        { deg: targetAngle },
+        {
+            duration: 5000,
+            step: function(now) {
+                const flapperPoint = (now - 30) % 60;
+                if (hasFlapped && flapperPoint > range) {
+                    hasFlapped = false;
+                }
+                if (!hasFlapped && now > 27 && now < 33) {
+                    flap();
+                    hasFlapped = true;
+                }
+                const inRange = (flapperPoint > -range && flapperPoint < range);
+                if (!hasFlapped && inRange && now > 60) {
+                    flap();
+                    hasFlapped = true;
+                }
+                $(this).css({ transform: `rotate(${now}deg)`});
+            }
+        }
+    );
+}
+
+$("#spin").click(function() {
+    spinWheel();
 });
